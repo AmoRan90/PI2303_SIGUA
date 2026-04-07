64f46a15514fc3ef4cc9cf473466fb58bbe34ae1
AmoRan90 <topprogamer90@gmail.com>
2026-04-07 20:53:44 +0300

    правки

--- coffee_machine/lib/interfaces/i_coffee.dart
+++
@@ -1,9 +0,0 @@
-import '../classes/resources.dart';
-
-abstract class ICoffee {
-  // Необходимые ресурсы для приготовления
-  Resources get requiredResources;
-  
-  // Стоимость напитка
-  int get cost;
-}
